121b51ff4862ff9ae599af9f6ef0b711d4e34415
Wilson Chen <wilson.chen@codeimmersives.com>
2020-02-19 15:47:35 -0500

    finish

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-function isHot() {
+function isHot(temp) {
+  if (temp >= 80){
+    return "Yes, it is indeed hot."
+  } else return 'No, it is not hot.'
+}
+
+
+function helloThere(name) {
+  if (name.length >= 6){
+    return 'Hi, '+name
+  }else return 'Hello, '+name
 
 }
 
 
-function helloThere() {
-
-}
-
-
-function goodbyeYou() {
+function goodbyeYou(name) {
+  
+if (name === undefined)
+  {return 'Goodbye, stranger'}
+  else return 'Goodbye, ' + name
 
 }
 
